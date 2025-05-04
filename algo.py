2fe273d005b866af9b5666820abbad52f06b0bf1
Maxime Girard <maxime.girard@lab6.co.jp>
2025-05-04 22:15:33 +0900

    Training look pass. I have to now pass a validation set.

---
+++ algo.py
@@ -0,0 +1,92 @@
+import json
+
+import tensorflow as tf
+#from tensorflow.keras import layers, models
+import numpy as np
+from PIL import Image
+
+# Parameters
+image_size = (80, 80)
+num_classes = 3
+channels = 1  # Set to 3 for RGB
+batch_size = 64
+epochs = 30
+
+map_labels = {'': 0,
+              'znr': 1,
+              'mrd': 2}
+
+with open('labels.json', 'r') as file:
+    labels = json.load(file)
+
+    imgs_train = np.zeros((len(labels), 80, 80, 3), dtype=np.float32)
+    labels_train = np.zeros((len(labels), 1), dtype=np.int32)
+
+    index = 0
+    for img_path, label in labels.items():
+        img = tf.keras.utils.load_img(img_path)
+        img_array = tf.keras.utils.img_to_array(img)
+        #img_array = img_array.reshape(-1, 3)
+        img_array = img_array / 255.0
+        imgs_train[index] = img_array
+
+        labels_train[index] = map_labels[label['exp_code']]
+
+        index = index + 1
+
+
+dataset: tf.data.Dataset = tf.data.Dataset.from_tensor_slices((imgs_train, labels_train))
+
+data_augmentation: tf.keras.Sequential = tf.keras.Sequential([
+    tf.keras.layers.RandomFlip("horizontal"),
+    tf.keras.layers.RandomRotation(0.1),
+    tf.keras.layers.RandomZoom(0.1),
+    tf.keras.layers.RandomContrast(0.2),
+])
+
+def augment(image: tf.Tensor, label: tf.Tensor) -> tuple[tf.Tensor, tf.Tensor]:
+    image = tf.cast(image, tf.float32)  # Ensure float for augmentations
+    image = data_augmentation(image, training=True)
+    return image, label
+
+augmented_ds: tf.data.Dataset = dataset.map(augment, num_parallel_calls=tf.data.AUTOTUNE)
+augmented_ds = augmented_ds.shuffle(1000).batch(32).prefetch(tf.data.AUTOTUNE)
+
+# Model
+model = tf.keras.Sequential([
+    #tf.keras.layers.Input(shape=(*image_size, channels)),
+
+    tf.keras.layers.Conv2D(32, kernel_size=3, padding='same', activation='relu', input_shape=(80, 80, 3)),
+    tf.keras.layers.MaxPooling2D(pool_size=2),  # 20x20
+
+    tf.keras.layers.Conv2D(64, kernel_size=3, padding='same', activation='relu'),
+    tf.keras.layers.MaxPooling2D(pool_size=2),  # 10x10
+
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(num_classes, activation='softmax'),
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Summary
+model.summary()
+
+class StopAtAccuracy(tf.keras.callbacks.Callback):
+    def __init__(self, target_acc: float):
+        super().__init__()
+        self.target_acc = target_acc
+
+    def on_epoch_end(self, epoch, logs=None):
+        current_acc = logs.get("accuracy")  # or "val_accuracy" for validation
+        if current_acc is not None and current_acc >= self.target_acc:
+            print(f"\n🚨 Target accuracy {self.target_acc} reached. Stopping training.")
+            self.model.stop_training = True
+
+# Train
+model.fit(augmented_ds,
+          epochs=epochs,
+          callbacks=[StopAtAccuracy(0.97)])
---
+++ crop.py
@@ -0,0 +1,103 @@
+import json
+
+from PIL import Image
+import os
+import random
+from typing import TextIO, Dict, List
+
+
+#exp_code_location_offset = {"mrd": (-10, 0), "znr": (10, 0)}
+exp_code_location_offset = {}
+
+
+def crop_image(exp_code: str, labels: Dict, rarity: str, color: str, illustration_id: str):
+    input_folder = f"mtg_images_{exp_code}"
+    output_folder = os.path.join("exp_code")
+    os.makedirs(output_folder, exist_ok=True)
+    height = 80
+    width = 80
+    x = 430 + exp_code_location_offset.get(exp_code, (0, 0))[0]
+    y = 400 + exp_code_location_offset.get(exp_code, (0, 0))[1]
+    assert(x + width / 2 < 488)
+
+    input_path = os.path.join(input_folder, illustration_id + '.jpg')
+    output_path = os.path.join(output_folder, f"{exp_code}_{rarity}_{color}.png")
+
+    if not os.path.exists(output_path):
+        for index in range(5):
+            while True:
+                rnd_x = random.randrange(0, 488, 1)
+                rnd_y = random.randrange(0, 680, 1)
+                if (((430 - 80) < rnd_x < (430 + 80)) and
+                        ((400 - 80) < rnd_y < (400 + 80)) or
+                        (rnd_x < 40) or
+                        ((488 - 40) < rnd_x) or
+                        (rnd_y < 40) or
+                        ((680 - 40) < rnd_y)):
+                    continue
+                break
+            output_path_error = os.path.join(output_folder, f"{exp_code}_{rarity}_{color}_error_{index}.png")
+            image = Image.open(input_path)
+            crop_box = (rnd_x - width / 2, rnd_y - height / 2, rnd_x + width / 2, rnd_y + height / 2)  # Example coordinates
+            cropped_image = image.crop(crop_box)
+            cropped_image.save(output_path_error)
+            labels[output_path_error.replace('\\', '/')] = {'exp_code': '', 'rarity': '', 'color': ''}
+
+    # Open and crop the image
+    image = Image.open(input_path)
+    crop_box = (x - width / 2, y - height / 2, x + width / 2, y + height / 2)  # Example coordinates
+    cropped_image = image.crop(crop_box)
+
+    # Save the cropped image
+    cropped_image.save(output_path)
+
+    labels[output_path.replace('\\', '/')] = {'exp_code': exp_code, 'rarity': rarity, 'color': color}
+
+    print(f"Processed: {illustration_id}")
+
+
+def crop_exp_code(exp_code: str, labels: Dict):
+    with open(f'result_{exp_code}.json', 'r') as file:
+        cards = json.load(file)
+
+        for card in cards:
+            rarity = card['rarity']
+
+            # Some cards have multiple faces
+            if "image_uris" in card:
+                # Single-faced card
+                colors = card['colors']
+                color = "none" if not colors else "multi" if len(colors) > 1 else colors[0]
+                crop_image(exp_code, labels, rarity, color, f"{card['illustration_id']}")
+            elif "card_faces" in card:
+                # Double-faced card
+                for i, face in enumerate(card["card_faces"]):
+                    if "image_uris" in face:
+                        colors = face['colors']
+                        color = "none" if not colors else "multi" if len(colors) > 1 else colors[0]
+                        crop_image(exp_code, labels, rarity, color, f"{face['illustration_id']}")
+
+
+def crop_exp_codes(exp_codes: List[str]):
+    labels = {}
+    for exp_code in exp_codes:
+        crop_exp_code(exp_code, labels)
+    with open('labels.json', 'w') as output:
+        json.dump(labels, output, indent=4)
+
+
+def get_crop_and_label(exp_code: str):
+    labels = {}
+    get_crop_exp_code(exp_code, labels)
+
+
+
+def crop():
+    exp_code = input(f"Which exp_code ? : ")
+    labels = {}
+    crop_exp_code(exp_code, labels)
+    pass
+
+
+if __name__ == "__main__":
+    crop_exp_codes(['mrd', 'znr'])
