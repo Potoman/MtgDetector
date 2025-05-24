818f0cdcb9a326a256f2324cee835c12a263ca8d
Potoman <lepotoman@msn.com>
2025-05-24 15:05:37 +0900

    Cache data to speed up processing.

--- card.py
+++ card.py
@@ -4,24 +4,29 @@ import algo
 import crop
 import json
 import random
-from typing import List
+from typing import Dict, List
+
+
+data_base: Dict[str, List[str]] = {}
 
 
 def get_illustrations_ids(exp_code: str) -> List[str]:
-    illustration_ids = []
-    with open(f'result_{exp_code}.json', 'r') as file:
-        cards = json.load(file)
-        for card in cards:
-            # Some cards have multiple faces
-            if "image_uris" in card:
-                # Single-faced card
-                illustration_ids.append(f"{card['illustration_id']}")
-            elif "card_faces" in card:
-                # Double-faced card
-                for i, face in enumerate(card["card_faces"]):
-                    if "image_uris" in face:
-                        illustration_ids.append(f"{face['illustration_id']}")
-    return illustration_ids
+    if exp_code not in data_base:
+        illustration_ids = []
+        with open(f'result_{exp_code}.json', 'r') as file:
+            cards = json.load(file)
+            for card in cards:
+                # Some cards have multiple faces
+                if "image_uris" in card:
+                    # Single-faced card
+                    illustration_ids.append(f"{card['illustration_id']}")
+                elif "card_faces" in card:
+                    # Double-faced card
+                    for i, face in enumerate(card["card_faces"]):
+                        if "image_uris" in face:
+                            illustration_ids.append(f"{face['illustration_id']}")
+        data_base[exp_code] = illustration_ids
+    return data_base[exp_code]
 
 
 def get_illustration_id_random(exp_code: str):
