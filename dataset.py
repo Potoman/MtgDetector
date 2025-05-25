b32d6e84c49d4ac02ffafb3bc4df278a2ad590b1
Potoman <lepotoman@msn.com>
2025-05-25 10:58:59 +0900

    Clean duplicate and remove presence flag as first step to develop.

--- dataset.py
+++ dataset.py
@@ -3,23 +3,39 @@ import cv2
 import noise
 import numpy as np
 import tensorflow as tf
+import tqdm
 
 
 def generate_data_set(count) -> tf.data.Dataset:
-    imgs_train = np.zeros((count, 800, 800, 3), dtype=np.float32)
-    labels_train = np.zeros((count, 9), dtype=np.int32)
+    imgs_train = np.zeros((count, 400, 400, 3), dtype=np.float32)
+    labels_train = np.zeros((count, 8), dtype=np.float32)
 
     exp_code: str = "mrd"
 
     index = 0
-    for index in range(count):
-        data, background_rgba = noise.get_noised_mtg_in_background(exp_code, card.get_illustration_id_random(exp_code))
-        background_bgr = cv2.cvtColor(background_rgba, cv2.COLOR_RGBA2BGR)
-        imgs_train[index] = background_bgr / 255.0
-        labels_train[index] = [1.0, data['x0'], data['x1'], data['x2'], data['x3'], data['y0'], data['y0'], data['y0'], data['y0']]
+    for index in tqdm.tqdm(range(count)):
+        img_train, label_train = get_image_and_keypoint(exp_code)
+        imgs_train[index] = img_train
+        labels_train[index] = label_train
         index = index + 1
 
     return tf.data.Dataset.from_tensor_slices((imgs_train, labels_train))
+
+
+def get_image_and_keypoint(exp_code: str):
+    data, background_rgba = noise.get_noised_mtg_in_background(exp_code, card.get_illustration_id_random(exp_code))
+    background_bgr = cv2.cvtColor(background_rgba, cv2.COLOR_RGBA2BGR)
+    background_bgr = cv2.resize(background_bgr, (400, 400), interpolation=cv2.INTER_LINEAR)
+    img_train = background_bgr / 255.0
+    label_train = [data['x0'] / 800.0,
+                   data['x1'] / 800.0,
+                   data['x2'] / 800.0,
+                   data['x3'] / 800.0,
+                   data['y0'] / 800.0,
+                   data['y1'] / 800.0,
+                   data['y2'] / 800.0,
+                   data['y3'] / 800.0]
+    return img_train, label_train
 
 
 if __name__ == '__main__':
